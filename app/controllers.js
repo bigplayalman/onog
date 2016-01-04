6f41f613a838a9fb7e5748ed14ee5d407d78ec8a
almasbarzhaxynov <almas@oomzie.com>
2016-01-04 01:44:30 -0500

    You can now auto generate brackets now

--- app/controllers.js
+++ app/controllers.js
@@ -33,17 +33,196 @@ var Controllers = angular.module('onog.controllers', [])
       relation.add(Parse.User.current());
       $scope.registeredList.push(bracket);
       $scope.availableList.splice(index, 1);
-
       bracket.set('registeredSlots', registered);
       bracket.save();
     }
   })
-  .controller('BracketDetailController', function($scope, $state, $stateParams, Parse, Bracket) {
+  .controller('BracketDetailController', function($scope, $state, $stateParams, Parse, Bracket, Shuffle, BracketRounds, Match, Round) {
     $scope.bracket = new Bracket();
+    $scope.players = [];
     $scope.bracket.id = $stateParams.id;
-    $scope.bracket.fetch().catch(function(err) {
+    $scope.bracket.fetch().then(function (bracket) {
+      $scope.bracket = bracket;
+      bracket.relation('players').query().find({
+        success: function(players) {
+          $scope.players = players;
+          $scope.displayBracket();
+        }
+      });
+    }).catch(function(err) {
       $state.go('viewTournaments');
-    });;
+    });
+
+    $scope.displayBracket = function () {
+      var query = new Parse.Query(Round);
+      query.equalTo('parent', $scope.bracket);
+      query.include('matches');
+      query.descending('roundNum');
+      query.find().then(function(rounds) {
+        $scope.rounds = rounds;
+      });
+    }
+
+    $scope.deleteRounds = function () {
+      var query = new Parse.Query(Round);
+      query.equalTo('parent', $scope.bracket);
+      return query.find({
+        success: function (rounds) {
+          Parse.Object.destroyAll(rounds);
+      }});
+    }
+    $scope.deleteMatches = function () {
+      var query = new Parse.Query(Match);
+      query.equalTo('bracket', $scope.bracket);
+      return query.find({
+        success: function (rounds) {
+          Parse.Object.destroyAll(rounds);
+        }});
+    }
+
+    $scope.createRounds = function () {
+      var players = $scope.players.length;
+      var numRounds = BracketRounds.getRounds(players);
+      var rounds  = [];
+      for(var i = numRounds; i >= 1; i--) {
+        var round = new Round();
+        var name = '';
+        var numGames = 0;
+        var remainder = 0;
+        if(players % 4 !== 0 && players > 4) {
+          remainder = 4 - (players % 4);
+        }
+        if(remainder === 2 && players > 4) {
+          numGames = 2;
+          name = 'Balance Round'
+          players = players + remainder;
+        } else if(remainder === 3 && players > 4) {
+          numGames = 1;
+          name = 'Balance Round'
+          players = players + remainder;
+        } else {
+          players = (players + remainder);
+          numGames = players/2;
+          name = $scope.getRoundName(i);
+        }
+
+        if(players === 2) {
+          numGames = 1;
+          name = 'Finals';
+        }
+
+        round.set('name', name);
+        round.set('numGames', numGames);
+        round.set('roundNum', i);
+        round.set('parent', $scope.bracket);
+        players = players/2;
+        rounds.push(round);
+      }
+      return Parse.Object.saveAll(rounds);
+    }
+
+    $scope.getRoundName = function (roundNum) {
+      switch(roundNum) {
+        case 1:
+          return 'Finals';
+          break;
+        case 2:
+          return 'Semifinals';
+          break;
+        case 3:
+          return 'Quarterfinals';
+          break;
+        default: return  'Round of ' + roundNum;
+      }
+    }
+
+    $scope.createMatches = function (list) {
+      var matches = [];
+      var players = Shuffle.shufflePlayers($scope.players.slice());
+      var remainder = 4 - (players.length % 4);
+
+      list.forEach(function(round) {
+        var matchCount = 1;
+        var currentRound = round;
+        while(matchCount <= round.get('numGames')) {
+
+
+          var match = new Match();
+          var playerOne = players[0];
+          var playerTwo = players[1];
+          var defaults = {
+            bracket: $scope.bracket,
+            score: null,
+            playerOne: null,
+            playerTwo: null,
+            winner: null,
+            loser: null,
+          }
+
+          match.set(defaults);
+          match.set('parent', currentRound);
+
+          if (players.length === 2) {
+            if(round.get('name') === 'Balance Round') {
+              match.set('playerOne', playerOne);
+              match.set('playerTwo', playerTwo);
+              players.splice(0, 2);
+            } else {
+              match.set('playerOne', playerOne);
+              players.splice(0, 1);
+            }
+          }
+
+          if(players.length === 1) {
+            if (remainder === 1) {
+              match.set('playerOne', playerOne);
+              match.set('winner', playerOne);
+              match.set('score', 'defwin');
+              remainder = 0;
+            } else if(matchCount === round.get('numGames')) {
+              match.set('playerTwo', playerOne);
+              players.splice(0, 1);
+            }
+          }
+
+          if(players.length > 2) {
+            match.set('playerOne', playerOne);
+            match.set('playerTwo', playerTwo);
+            players.splice(0, 2);
+          }
+
+          matchCount++;
+          matches.push(match);
+        }
+      });
+      return Parse.Object.saveAll(matches);
+    }
+    $scope.updateRounds = function (matches) {
+      var rounds = [];
+      matches.forEach(function(match) {
+        var game = new Match();
+        game.id = match.id;
+        var round = new Round();
+        round.id = match.parent.id;
+        round.add('matches', game);
+        rounds.push(round);
+      });
+      return Parse.Object.saveAll(rounds);
+    }
+
+    $scope.generateBracket = function () {
+      $scope.deleteRounds().then(function(){
+        $scope.createRounds().then(function (rounds) {
+          $scope.deleteMatches().then(function () {
+            $scope.createMatches(rounds).then(function(matches) {
+              $scope.updateRounds(matches).then(function(rounds) {
+                $scope.displayBracket();
+              });
+            });
+          });
+        });
+      });
+    }
   })
   .controller('CreateBracketController', function($scope, Parse, Bracket) {
     $scope.bracket = new Bracket();
--- app/services.js
+++ app/services.js
@@ -14,6 +14,54 @@ angular.module('onog.services', []).run(function ($http) {
       getNewUser : getNewUser
     };
   })
+  .service('BracketRounds', function () {
+    var getRounds = function (length) {
+      var rounds = 0;
+      var remainder = 4 - (length % 4);
+      var players = length + remainder
+      for(var i = 0; i <= players; i += 4) {
+        rounds++;
+      }
+      return rounds;
+    }
+    return {
+      getRounds : getRounds
+    }
+  })
+  .service('Shuffle', function () {
+    var shufflePlayers = function (array) {
+      var m = array.length, t, i;
+
+      // While there remain elements to shuffle…
+      while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+      }
+
+      return array;
+    }
+    return {
+      shufflePlayers : shufflePlayers
+    }
+  })
+  .factory('Round', ['Parse', function (Parse) {
+    var Round = Parse.Object.extend('Round');
+    Parse.defineAttributes(Round, ['matches', 'name', 'parent', 'roundNum']);
+
+    return Round;
+  }])
+  .factory('Match', ['Parse', function (Parse) {
+    var Match = Parse.Object.extend('Match');
+    Parse.defineAttributes(Match, ['playerOne', 'playerTwo', 'parent', 'score', 'winner', 'loser', 'bracket']);
+
+    return Match;
+  }])
   .factory('Bracket', ['Parse', function (Parse) {
     var Bracket = Parse.Object.extend('Bracket');
     Parse.defineAttributes(Bracket, ['name', 'type', 'game', 'totalSlots', 'registeredSlots']);
